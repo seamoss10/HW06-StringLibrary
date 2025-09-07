bc76fae21fc1191edd5d807f2b21ec5dcd067f3b
Daniel Soe <thetnsoe1@gmail.com>
2025-09-07 18:48:21 -0400

    HW7 done

--- stringm.c
+++ stringm.c
@@ -16,7 +16,7 @@
 size_t strlen_m(const char *string) {
 
     size_t count = 0;
-    while(*string != '\0') {
+    while (*string != '\0') {
         count++;
         string++;
     }
@@ -34,14 +34,10 @@ size_t strlen_m(const char *string) {
 */
 char *strncpy_m(const char *string, size_t n) {
 
-    char * copy;
-    copy = (char *) malloc((n + 1) * sizeof(char));
-    if (copy == NULL) {
-        return NULL;
-    }
+    char * copy = (char *) malloc((n + 1) * sizeof(char));
 
     int i = 0;
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         copy[i] = string[i];
     }
     copy[i] = '\0';
@@ -64,22 +60,18 @@ char *join_m(Strings strings, const char *delimiter) {
 
     int total_size = 0;
     int delim_size = (strings.num_strings - 1) * strlen_m(delimiter);
-    for(int i = 0; i < strings.num_strings; i++) {
+    for (int i = 0; i < strings.num_strings; i++) {
         total_size += strlen_m(strings.strings[i]);
     }
 
-    char * join;
-    join = (char *) malloc((total_size + delim_size + 1) * sizeof(char));
-    if (join == NULL) {
-        return NULL;
-    }
+    char * join = (char *) malloc((total_size + delim_size + 1) * sizeof(char));
 
     int pos;
-    for(pos = 0; pos < strlen_m(strings.strings[0]); pos++) {
+    for (pos = 0; pos < strlen_m(strings.strings[0]); pos++) {
         join[pos] = strings.strings[0][pos];
     }
 
-    for(int i = 1; i < strings.num_strings; i++) {
+    for (int i = 1; i < strings.num_strings; i++) {
         for(int j = 0; j < strlen_m(delimiter); j++) {
             join[pos++] = delimiter[j];
         }
@@ -87,7 +79,8 @@ char *join_m(Strings strings, const char *delimiter) {
         for(int j = 0; j < strlen_m(strings.strings[i]); j++) {
             join[pos++] = strings.strings[i][j];
         }
-    }    
+    }
+    join[pos] = '\0'; 
 
     return join;
 }
@@ -97,7 +90,7 @@ char *join_m(Strings strings, const char *delimiter) {
 ** String strings - free each string in strings.strings and strings.strings itself
 */
 void free_strings(Strings strings) {
-    for(int i = 0; i < strings.num_strings; i++) {
+    for (int i = 0; i < strings.num_strings; i++) {
         free(strings.strings[i]);
     }       
     free(strings.strings);
@@ -122,9 +115,39 @@ void free_strings(Strings strings) {
 **           or NULL if not found
 **
 */
-Strings split_m(const char *string, const char *pattern)
-{
+Strings split_m(const char *string, const char *pattern) {
     Strings result = { .num_strings = 0, .strings = NULL };
+
+    int pattern_len = strlen_m(pattern);
+    if (pattern_len == 0) {
+        result.num_strings = 1;
+        result.strings = malloc(1 * sizeof(char *));
+        result.strings[0] = strncpy_m(string, strlen_m(string));
+        return result;
+    }
+    
+    int count = 0;
+    const char * curr = string;
+    const char * delim_pos = NULL;
+
+    while ((delim_pos = strstr_m(curr, pattern)) != NULL) {
+        curr = delim_pos + pattern_len;
+        count++;
+    }
+    result.num_strings = count + 1;
+    result.strings = malloc(result.num_strings * sizeof(char *));
+
+    const char * ptr = string;
+    const char * pos = NULL;
+    int i = 0;
+    while ((pos = strstr_m(ptr, pattern)) != NULL) {
+        int substr_len = pos - ptr;
+        char * substr = strncpy_m(ptr, substr_len);
+        result.strings[i++] = substr;
+        ptr = pos + pattern_len;
+    }
+    result.strings[i] = strncpy_m(ptr, strlen_m(ptr));
+
     return result;
 }
 
@@ -139,9 +162,13 @@ Strings split_m(const char *string, const char *pattern)
 **       If pattern is "", return a malloc'd copy of 'string' unchanged (no insertions of 'replacement').
 ** hint: there are two main ways of implementing this function, one involves many lines, one involves four
 */
-char *find_and_replace_all_m(const char *string, const char *pattern, const char *replacement)
-{
-    return NULL;
+char *find_and_replace_all_m(const char *string, const char *pattern, const char *replacement) {
+    if (strlen_m(pattern) == 0) {
+        char * copy = malloc(strlen_m(string) * sizeof(char));
+        copy = strncpy_m(string, strlen_m(string));
+        return copy;
+    }
+    return join_m(split_m(string, pattern), replacement);
 }
 
 /*
